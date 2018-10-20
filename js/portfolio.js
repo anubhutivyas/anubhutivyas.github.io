72145572e4e90ba91957e332fb36092f7a767589
Anubhuti Vyas <vyas.an@husky.neu.edu>
2018-10-19 23:37:15 -0400

    Update CSS and add section

--- js/portfolio.js
+++ js/portfolio.js
@@ -3,6 +3,10 @@ $(document).ready(function(){
     var $animation_elements = $('.anim');
     var $window = $(window);
 
+    var $circles = $('.radial-progress');
+
+    var $bars = $('.animateBar');
+
     function check_if_in_view() {
         var window_height = $window.height();
         var window_top_position = $window.scrollTop();
@@ -22,26 +26,57 @@ $(document).ready(function(){
                 $element.removeClass('animated');
             }
         });
+
+        $.each($circles, function() {
+            var $element = $(this);
+            var element_height = $element.outerHeight();
+            var element_top_position = $element.offset().top;
+            var element_bottom_position = (element_top_position + element_height);
+
+//check to see if this current container is within viewport
+            if ((element_bottom_position >= window_top_position) &&
+                (element_top_position <= window_bottom_position)) {
+                setTimeout(circleAnimate, 400);
+            }
+        });
+
+        $.each($bars, function() {
+            var $element = $(this);
+            var element_height = $element.outerHeight();
+            var element_top_position = $element.offset().top;
+            var element_bottom_position = (element_top_position + element_height);
+
+//check to see if this current container is within viewport
+            if ((element_bottom_position >= window_top_position) &&
+                (element_top_position <= window_bottom_position)) {
+                $element.addClass('bar');
+            }
+            else{
+                $element.removeClass('bar');
+            }
+        });
     }
 
     $window.on('scroll resize', check_if_in_view);
     $window.trigger('scroll');
 });
-/**********************Scroll Animation "END"************************************/
 
-/**********************Change color of center aligned animated content small Circle  "START"************************************/
-$(document).ready(function(){
-    $(" .debits").hover(function(){
-        $(" .center-right").css("background-color", "#4997cd");
-    }, function(){
-        $(" .center-right").css("background-color", "#fff");
+
+
+function circleAnimate() {
+    $('.radial-progress').each(function() {
+        var transform_styles = ['-webkit-transform', '-ms-transform', 'transform'];
+        $(this).find('span').fadeTo('slow', 1);
+        var score = $(this).data('score');
+        var deg = (((100 / 5) * score) / 100) * 180;
+        var rotation = deg;
+        var fill_rotation = rotation;
+        var fix_rotation = rotation * 2;
+        for(i in transform_styles) {
+            $(this).find('.circle .fill, .circle .mask.full').css(transform_styles[i], 'rotate(' + fill_rotation + 'deg)');
+            $(this).find('.circle .fill.fix').css(transform_styles[i], 'rotate(' + fix_rotation + 'deg)');
+        }
     });
-});
-$(document).ready(function(){
-    $(".credits").hover(function(){
-        $(".center-left").css("background-color", "#4997cd");
-    }, function(){
-        $(".center-left").css("background-color", "#fff");
-    });
-});
-/**********************Change color of center aligned animated content small Circle  "END"************************************/
+}
+
+
